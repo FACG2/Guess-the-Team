b4a25a26b9d41ab557cee12576459c1c85b37193
Abdallah <you@example.com>
2017-07-26 14:32:54 +0300

    add answering logic

--- js/team.js
+++ js/team.js
@@ -14,7 +14,14 @@
 
    getRandomTeam: function(cb) {
       this.getTeamsList(function(res){
-       var team = res[Math.floor(Math.random() * 20) + 1].shortName;
+       var team = res[Math.floor(Math.random() * 20)].shortName;
+       if (team==="ManCity") team = "Manchester City";
+       else if (team==="Crystal") team = "Crystal Palace";
+       else if(team==="West Ham") team = "WestHam";
+       else if(team==="ManU") team = "Manchester United";
+       else if(team==="Swans") team = "Swansea City";
+       else if(team==="Spurs") team = "Tottenham";
+       else if(team==="Foxes") team = "Leicester City";
         cb(team);
    })
  }
